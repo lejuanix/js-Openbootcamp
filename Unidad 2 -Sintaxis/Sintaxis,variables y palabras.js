9b10d499cd1dbace1568281ed3d5ad67103bbe05
Juan Ignacio Gutierrez <107145193+lejuanix@users.noreply.github.com>
2023-05-02 16:07:33 -0300

    Add files via upload

---
+++ Unidad 2 -Sintaxis/Sintaxis,variables y palabras.js
@@ -0,0 +1,10 @@
+const libro = {
+    Titulo: "Carrie",
+    Autor: "Stephen King",
+    fecha: "27 de octubre de 1983",
+    url: "https://es.wikipedia.org/wiki/Carrie_(novela)",
+}
+
+const lista = ["Juan Ignacio Gutierrez", 29, false, Date(9, 4, 1993), libro]
+
+console.log(lista);
